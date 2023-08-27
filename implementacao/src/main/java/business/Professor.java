5145809b8d5aa85a315b7dc075b210b138868369
Luizgms <luiz.mendes@sga.pucminas.br>
2023-08-27 17:16:42 -0300

    Criação Classes Professor e ISalvave

---
+++ implementacao/src/main/java/business/Professor.java
@@ -0,0 +1,11 @@
+package business;
+
+public class Professor extends Usuario implements ISalvavel {
+    Professor(String nome, String senha) {
+        super(nome, senha);
+
+    }
+
+    public void getDados() {
+    }
+}
