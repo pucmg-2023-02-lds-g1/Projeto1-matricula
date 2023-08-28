ba185ab384ea66f160c93b05f953aff7a6a16d63
Drumond <drumond.work@gmail.com>
2023-08-28 16:51:34 -0300

    Metodo carregarUsuarios feito

--- implementacao/src/main/java/business/SistemaMatricula.java
+++ implementacao/src/main/java/business/SistemaMatricula.java
@@ -1,14 +1,28 @@
 package business;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class SistemaMatricula {
     
-    public String nome;
+    private String nome;
+    private static final String arqUsuario = "implementacao\\src\\arquivos\\arqUsuarios.txt";
+    private HashMap<String, Usuario> usuarios = new HashMap<>();
 
     public SistemaMatricula(String nome){
+
+        carregarUsuario();
+
         if(nome.length() > 1){
             this.nome = nome;
         }
     }
+
 
     public Disciplina matricularDisciplina(String nome, String nomeDisciplina){
 
@@ -39,5 +53,38 @@ public class SistemaMatricula {
 
     public void cadastro(){
 
+    }
+
+    private void carregarUsuario() {
+        try {
+
+            Usuario usuario;
+
+            BufferedReader reader = new BufferedReader(new FileReader(arqUsuario));
+            String linha;
+            reader.readLine();
+
+            while ((linha = reader.readLine()) != null) {
+                StringTokenizer str = new StringTokenizer(linha, ";");
+                String tipo = str.nextToken();
+                String nome = str.nextToken();
+                String senha = str.nextToken();
+
+                if(tipo.equals("A")) {
+                    usuario = new Aluno(nome, senha);
+                } else if(tipo.equals("P")) {
+                    usuario = new Professor(nome, senha);
+                } else {
+                    usuario = new Secretario(nome, senha);
+                }
+
+                usuarios.put(nome, usuario);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
