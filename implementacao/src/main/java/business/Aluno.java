21016b843240862cac1baf144053d4ab05fb6799
AmandaMS09 <mandanevs@gmail.com>
2023-08-30 22:02:55 -0300

    Remoção da lista de disciplinas - Aluno.java #37

--- implementacao/src/main/java/business/Aluno.java
+++ implementacao/src/main/java/business/Aluno.java
@@ -1,27 +1,10 @@
 package business;
 
-import java.util.List;
-
 public class Aluno extends Usuario {
-    private List<Disciplina> disciplinas;
 
     Aluno(String nome, String senha) {
         super(nome, senha);
     }
-
-    public void addDisciplinas(Disciplina disciplina){
-        getDisciplinas().add(disciplina);
-        disciplina.addAlunos(this);
-    }
-
-    public void removeDisciplinas(Disciplina disciplina){
-        getDisciplinas().remove(disciplina);
-    }
-
-    public List<Disciplina> getDisciplinas() {
-        return this.disciplinas;
-    }
-
     
     public String visualizarCobranca(SistemaCobranca financeiro){
 
